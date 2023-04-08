31763ce785fc275aa98841dd0d21f0672e29261c
ade pranaya <ade.pranaya@mail.unpas.ac.id>
2023-04-08 08:22:09 +0700

    belajar tools third party

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.[contenthash].js',
-    clean: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
     }),
   ],
-  watch: true,
-  devtool: false,
   module: {
     rules: [
       // {
