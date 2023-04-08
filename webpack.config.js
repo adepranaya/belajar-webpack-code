1da677e20c23d3440fba9c9d2458b9fe96cadcce
ade pranaya <ade.pranaya@mail.unpas.ac.id>
2023-04-08 07:49:10 +0700

    belajar loders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
-const path = require('path')
+const path = require('path');
 
-module.exports =  {
-  mode: 'production',
+module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'output'),
-    filename: 'bundle.js'
-  }
-}
+    filename: 'bundle.js',
+  },
+  watch: true,
+  devtool: false,
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/i,
+      //   use: ['style-loader', 'css-loader'],
+      // },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
